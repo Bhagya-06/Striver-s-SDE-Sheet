a1781328a86e1c50cac5c3c85312300c95b1370c
Bhagya B <114055494+Bhagya-06@users.noreply.github.com>
2025-04-09 20:50:02 +0530

    Create Sort Colors (Bubble Sort).c

---
+++ Sort Colors (Bubble Sort).c
@@ -0,0 +1,37 @@
+/* Bubble Sort Algorithm:
+- i loop to iterate through array and each time largest element goes to last
+- j loop to compare elements from start to (n-i-1) since i number of elements are already sorted at the last 
+- if some element in j loop is greater than its next element then swap it
+- Repeat until all elements are sorted */
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
